cfcb854a436a8cfa191f28e8117689ec5c6a2f5d
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-22 11:34:34 +0100

    Milestone 1

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import AboutPage from './pages/AboutPage';
+import ProductsPage from './pages/ProductsPage';
+import ProductDetailPage from "./pages/ProductDetailPage";
+import { BudgetProvider } from "./contexts/BudgetContext.jsx";
+
+
+
+
+export default function App() {
+  return (
+    <BudgetProvider>
+      <BrowserRouter>
+
+        <Navbar />
+
+        <div className="container mt-4">
+          <Routes>
+
+            <Route path="/" element={<HomePage />} />
+            <Route path="/chi-siamo" element={<AboutPage />} />
+            <Route path="/prodotti" element={<ProductsPage />} />
+            <Route path="/prodotti/:id" element={<ProductDetailPage />} />
+
+          </Routes>
+        </div>
+
+      </BrowserRouter>
+    </BudgetProvider>
+  )
+}
---
+++ src/contexts/BudgetContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const BudgetContext = createContext()
+
+export function BudgetProvider({ children }) {
+    const [budgetMode, setBudgetMode] = useState(false)
+
+    return (
+
+        <BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+            {children}
+        </BudgetContext.Provider>
+    )
+
+
+}
